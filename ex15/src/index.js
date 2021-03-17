066bfcdab9dfebbe2a3ae0bee3822e46ec473643
enida <enida_mujezin_16yahoo.com>
2021-03-17 09:40:06 +0100

    ex15

---
+++ ex15/src/index.js
@@ -0,0 +1,13 @@
+function myDowhile(){
+var myNumbers;
+myNumbers =0;
+do {
+    console.log(myNumbers);
+    myNumbers++;
+} while (myNumbers < 10);
+
+};
+
+console.log(myDowhile());
+
+module.exports = myDowhile;
