7ee5ab505086bb722abf0baeba94c3eb906d4cab
enida <enida_mujezin_16yahoo.com>
2021-03-17 09:30:43 +0100

    ex04

---
+++ ex04/src/index.js
@@ -0,0 +1,15 @@
+function compareDifferentValues(m, n) {
+    if (typeof m === typeof n) {
+        return "Equal"
+    }
+    return "Not equal"
+};
+
+console.log(compareDifferentValues(8, "8"))
+console.log(compareDifferentValues("8", 8))
+console.log(compareDifferentValues('8', 8))
+console.log(compareDifferentValues("8", "8"))
+console.log(compareDifferentValues(8, 8))
+
+
+module.exports = compareDifferentValues;
