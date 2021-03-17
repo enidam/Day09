ccd0c73e713971660ebe1ec7907ed741a10df77b
enida <enida_mujezin_16yahoo.com>
2021-03-17 09:37:08 +0100

    ex13

---
+++ ex13/src/index.js
@@ -0,0 +1,11 @@
+function inversWhile(){
+
+    var fiveNumbers;
+    fiveNumbers = 5;
+    while(fiveNumbers > -1 ){
+        console.log(fiveNumbers);
+        fiveNumbers--;
+    }
+};
+console.log(inversWhile());
+module.exports = inversWhile;
